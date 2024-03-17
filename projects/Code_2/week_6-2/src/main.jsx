31525165d25d4e5a3255994767842b53cb4a4984
EAzZY-1wnL <madhur09112001@gmail.com>
2024-03-17 22:11:45 +0530

    new project

--- projects/Code_2/week_6-2/src/main.jsx
+++ projects/Code_2/week_6-2/src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import App1 from './app1.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    // <App></App>
-    <App1></App1>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
